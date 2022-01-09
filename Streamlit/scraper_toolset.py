710b237267575610b191a7ce5de6ae58ec93a012
kvardhan10 <kanishkvardhanan@gmail.com>
2022-01-09 21:02:04 +0530

    implemented sleep

--- Streamlit/scraper_toolset.py
+++ Streamlit/scraper_toolset.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 from bs4 import BeautifulSoup
-# import numpy as np
+import time as tm
 from matplotlib import pyplot as plt
 import  wikipedia
 import yfinance as yf
@@ -23,23 +23,27 @@ def covidDetails():
 
     results=[{headers[index]:cell.text for index,cell in enumerate(row.find_all("td")) }for row in table_rows]
     name = st.selectbox('Choose country', [ 'World','Afghanistan', 'Aland Islands', 'Albania', 'Algeria', 'American Samoa', 'Andorra', 'Angola', 'Anguilla', 'Antarctica', 'Antigua and Barbuda', 'Argentina', 'Armenia', 'Aruba', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas', 'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bermuda', 'Bhutan', 'Bolivia, Plurinational State of', 'Bonaire, Sint Eustatius and Saba', 'Bosnia and Herzegovina', 'Botswana', 'Bouvet Island', 'Brazil', 'British Indian Ocean Territory', 'Brunei Darussalam', 'Bulgaria', 'Burkina Faso', 'Burundi', 'Cambodia', 'Cameroon', 'Canada', 'Cape Verde', 'Cayman Islands', 'Central African Republic', 'Chad', 'Chile', 'China', 'Christmas Island', 'Cocos (Keeling) Islands', 'Colombia', 'Comoros', 'Congo', 'Congo, The Democratic Republic of the', 'Cook Islands', 'Costa Rica', "Côte d'Ivoire", 'Croatia', 'Cuba', 'Curaçao', 'Cyprus', 'Czech Republic', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic', 'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea', 'Eritrea', 'Estonia', 'Ethiopia', 'Falkland Islands (Malvinas)', 'Faroe Islands', 'Fiji', 'Finland', 'France', 'French Guiana', 'French Polynesia', 'French Southern Territories', 'Gabon', 'Gambia', 'Georgia', 'Germany', 'Ghana', 'Gibraltar', 'Greece', 'Greenland', 'Grenada', 'Guadeloupe', 'Guam', 'Guatemala', 'Guernsey', 'Guinea', 'Guinea-Bissau', 'Guyana', 'Haiti', 'Heard Island and McDonald Islands', 'Holy See (Vatican City State)', 'Honduras', 'Hong Kong', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iran, Islamic Republic of', 'Iraq', 'Ireland', 'Isle of Man', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jersey', 'Jordan', 'Kazakhstan', 'Kenya', 'Kiribati', "Korea, Democratic People's Republic of", 'Korea, Republic of', 'Kuwait', 'Kyrgyzstan', "Lao People's Democratic Republic", 'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Macao', 'Macedonia, Republic of', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta', 'Marshall Islands', 'Martinique', 'Mauritania', 'Mauritius', 'Mayotte', 'Mexico', 'Micronesia, Federated States of', 'Moldova, Republic of', 'Monaco', 'Mongolia', 'Montenegro', 'Montserrat', 'Morocco', 'Mozambique', 'Myanmar', 'Namibia', 'Nauru', 'Nepal', 'Netherlands', 'New Caledonia', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'Niue', 'Norfolk Island', 'Northern Mariana Islands', 'Norway', 'Oman', 'Pakistan', 'Palau', 'Palestinian Territory, Occupied', 'Panama', 'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines', 'Pitcairn', 'Poland', 'Portugal', 'Puerto Rico', 'Qatar', 'Réunion', 'Romania', 'Russian Federation', 'Rwanda', 'Saint Barthélemy', 'Saint Helena, Ascension and Tristan da Cunha', 'Saint Kitts and Nevis', 'Saint Lucia', 'Saint Martin (French part)', 'Saint Pierre and Miquelon', 'Saint Vincent and the Grenadines', 'Samoa', 'San Marino', 'Sao Tome and Principe', 'Saudi Arabia', 'Senegal', 'Serbia', 'Seychelles', 'Sierra Leone', 'Singapore', 'Sint Maarten (Dutch part)', 'Slovakia', 'Slovenia', 'Solomon Islands', 'Somalia', 'South Africa', 'South Georgia and the South Sandwich Islands', 'Spain', 'Sri Lanka', 'Sudan', 'Suriname', 'South Sudan', 'Svalbard and Jan Mayen', 'Swaziland', 'Sweden', 'Switzerland', 'Syrian Arab Republic', 'Taiwan, Province of China', 'Tajikistan', 'Tanzania, United Republic of', 'Thailand', 'Timor-Leste', 'Togo', 'Tokelau', 'Tonga', 'Trinidad and Tobago', 'Tunisia', 'Turkey', 'Turkmenistan', 'Turks and Caicos Islands', 'Tuvalu', 'Uganda', 'Ukraine', 'United Arab Emirates', 'UK', 'USA', 'United States Minor Outlying Islands', 'Uruguay', 'Uzbekistan', 'Vanuatu', 'Venezuela, Bolivarian Republic of', 'Viet Nam', 'Virgin Islands, British', 'Virgin Islands, U.S.', 'Wallis and Futuna', 'Yemen', 'Zambia', 'Zimbabwe'])
-    for i in results:
-        if "Country" in i:
-            if i["Country"]==name:
-                st.write(i)
-                qty = [float(i['TotalCases'].replace(',', '')), float(i['TotalDeaths'].replace(',', '')), float(i['TotalRecovered'].replace(',', '')), float(i['ActiveCases'].replace(',', '')), float(i['Serious,Critical'].replace(',', ''))]
-                of = ['TotalCases', 'TotalDeaths', 'TotalRecovered', 'ActiveCases', 'Serious,Critical']
-                explode = (0.1, 0.1, 0.1, 0.1, 0.1)
-                fig1, ax1 = plt.subplots()
-                ax1.pie(qty, explode=explode, labels=of, autopct='%1.1f%%', radius=2)
-                st.pyplot(fig1)
+    with st.spinner(text='In progress'):
+        tm.sleep(3)
+        for i in results:
+            if "Country" in i:
+                if i["Country"]==name:
+                    st.write(i)
+                    qty = [float(i['TotalCases'].replace(',', '')), float(i['TotalDeaths'].replace(',', '')), float(i['TotalRecovered'].replace(',', '')), float(i['ActiveCases'].replace(',', '')), float(i['Serious,Critical'].replace(',', ''))]
+                    of = ['TotalCases', 'TotalDeaths', 'TotalRecovered', 'ActiveCases', 'Serious,Critical']
+                    explode = (0.1, 0.1, 0.1, 0.1, 0.1)
+                    fig1, ax1 = plt.subplots()
+                    ax1.pie(qty, explode=explode, labels=of, autopct='%1.1f%%', radius=2)
+                    st.pyplot(fig1)
 
 def wikiSumm():
     st.header('Wikipedia Summarizer')
     value = st.text_input('Search what you want??')
     values = wikipedia.summary(value)
     # engine = pyttsx3.init()
-    st.write(values)
+    with st.spinner(text='In progress'):
+        tm.sleep(3)
+        st.write(values)
 
 def weather():
     st.header('Weather Details')
@@ -66,11 +70,14 @@ def weather():
     other_data = strd[pos:]
 
     # printing all data
-    st.write(city," weather details are as follows\n");
-    st.write("Temperature is", temp)
-    st.write("Time is ", time)
-    st.write("Sky Description: ", sky)
-    # st.write(other_data)
+    with st.spinner(text='In progress'):
+        tm.sleep(3)
+
+        st.write(city," weather details are as follows\n");
+        st.metric("Temperature is", temp)
+        st.metric("Time is ", time)
+        st.metric("Sky Description: ", sky)
+        # st.write(other_data)
 
 def stocks():
     st.header('STOCK PRICING 101')
@@ -78,45 +85,47 @@ def stocks():
     tickerData = yf.Ticker(tickerSymbol)
     tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
 
-    st.write("""
+    with st.spinner(text='In progress'):
+        tm.sleep(3)
+        st.write("""
     ### Closing price
     """)
-    st.line_chart(tickerDf.Close)
+        st.line_chart(tickerDf.Close)
 
-    st.write("""
+        st.write("""
     ### Volume price
     """)
-    st.line_chart(tickerDf.Volume)
+        st.line_chart(tickerDf.Volume)
 
-    st.write("""
+        st.write("""
     ### Open price
     """)
-    st.line_chart(tickerDf.Open)
+        st.line_chart(tickerDf.Open)
 
-    st.write("""
+        st.write("""
     ### Low price
     """)
-    st.line_chart(tickerDf.Low)
+        st.line_chart(tickerDf.Low)
 
-    st.write("""
+        st.write("""
     ### High price
     """)
-    st.line_chart(tickerDf.High)
+        st.line_chart(tickerDf.High)
 
-    st.write("""
+        st.write("""
     ### Dividends
     """)
-    st.line_chart(tickerDf.Dividends)
+        st.line_chart(tickerDf.Dividends)
 
-    st.write("""
+        st.write("""
     ### Recommendations
     """)
-    tickerData.recommendations
+        tickerData.recommendations
 
-    st.write("""
+        st.write("""
     ### Company Data
     """)
-    tickerData.info
+        tickerData.info
 
 def horoscope():
     st.subheader('Horoscope')
@@ -161,7 +170,9 @@ def horoscope():
 
         for item in data:
             res = item.p.text
-        st.write(res)
+        with st.spinner(text='In progress'):
+            tm.sleep(3)
+            st.write(res)
 
     a = st.selectbox('Pick your Zodiac Sign', zodiac_sign)
     b = st.selectbox('Pick time value', day)
